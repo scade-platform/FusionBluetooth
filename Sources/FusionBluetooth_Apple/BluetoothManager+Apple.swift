681e61c2661c8b05c375d4a6ec764ac8e50e266a
Pavlo Boiko <boiko.impul@gmail.com>
2021-08-16 23:20:52 +0300

    update iOS

--- Sources/FusionBluetooth_Apple/BluetoothManager+Apple.swift
+++ Sources/FusionBluetooth_Apple/BluetoothManager+Apple.swift
@@ -6,137 +6,192 @@ public class BluetoothManager {
     fileprivate class CBCDelegate: NSObject {
         typealias Receiver = (Peripheral?) -> Void
         typealias StateReceiver = (DeviceState) -> Void
+        typealias DataReceiver = (Data?) -> Void
         var receiver: Receiver?
         var stateReceiver: StateReceiver?
+        var readCharacteristicReceiver: DataReceiver?
+        var notifyCharacteristicReceiver: DataReceiver?
+        var writeCharacteristicReceiver: DataReceiver?
         
         var peripheralArray: [CBPeripheral] = []
+        var writeData: Data?
     }
   
-	private let delegate: CBCDelegate
-	private let centralManager: CBCentralManager
-//	private let peripheral: CBPeripheral!
-	
-	public required init() {
-		self.delegate = CBCDelegate()
+    private let delegate: CBCDelegate
+    private let centralManager: CBCentralManager
+    
+    public required init() {
+        self.delegate = CBCDelegate()
         self.centralManager = CBCentralManager(delegate: self.delegate, queue: nil)
-    }	
+    }
 }
 
 extension BluetoothManager: BluetoothManagerProtocol {
-	public func requestAuthorization() { }
-	
-	public func isSupporting() -> Bool {
+    public func requestAuthorization() { }
+    
+    public func isSupporting() -> Bool {
         return self.centralManager.state != .unsupported
-	}
-		
-	public func isDiscovering() -> Bool {
-		return self.centralManager.isScanning		
-	}
-	
-	public func isEnabled() -> Bool {
-        return self.centralManager.state == .poweredOn
-	}
-	
-	public func startDiscovering(receiver: @escaping (Peripheral?) -> Void) {
-		self.delegate.receiver = receiver
-		centralManager.scanForPeripherals(withServices: nil, options: nil)
-	}
-	
-	public func stopDiscovering() {
-		centralManager.stopScan()
-	}
-	
-	public func connectDevice(uuid: String, receiver: @escaping (Peripheral?) -> Void) {	
-		self.delegate.receiver = receiver
-		if let peripheral = self.delegate.peripheralArray.first(where: { "\($0.identifier)" == uuid }) {
-            centralManager.connect(peripheral, options: nil)
-        } else {
-        	receiver(nil)
-        }
-	}
-	
-	public func disconnectDevice(uuid: String, receiver: @escaping (Peripheral?) -> Void) {
-		self.delegate.receiver = receiver
-		if let peripheral = self.delegate.peripheralArray.first(where: { uuid == "\($0.identifier)" }) {
-            centralManager.cancelPeripheralConnection(peripheral)
-        } else {
-        	receiver(nil)
-        }
-	}
-	
-	public func enableBluetooth() {		
-	}
-	
-	public func receiveMessage(message: @escaping (String) -> Void) {
-		
-	}
-	
-    public func sendMessage(message: String) {
-    	
+    }
+        
+    public func isDiscovering() -> Bool {
+        return self.centralManager.isScanning
     }
     
-	public func listenToStateEvents(receiver: @escaping (DeviceState) -> Void) {
-		self.delegate.stateReceiver = receiver
-	}
+    public func isEnabled() -> Bool {
+        return self.centralManager.state == .poweredOn
+    }
+    
+    public func startDiscovering(receiver: @escaping (Peripheral?) -> Void) {
+        self.delegate.receiver = receiver
+        centralManager.scanForPeripherals(withServices: nil, options: nil)
+    }
+    
+    public func stopDiscovering() {
+        centralManager.stopScan()
+    }
+    
+    public func connectDevice(uuid: String, receiver: @escaping (Peripheral?) -> Void) {
+        self.delegate.receiver = receiver
+        if let peripheral = self.delegate.peripheralArray.first(where: { "\($0.identifier)" == uuid }) {
+            centralManager.connect(peripheral, options: nil)
+        } else {
+            receiver(nil)
+        }
+    }
+    
+    public func disconnectDevice(uuid: String, receiver: @escaping (Peripheral?) -> Void) {
+        self.delegate.receiver = receiver
+        if let peripheral = self.delegate.peripheralArray.first(where: { uuid == "\($0.identifier)" }) {
+            centralManager.cancelPeripheralConnection(peripheral)
+        } else {
+            receiver(nil)
+        }
+    }
+    
+    public func enableBluetooth() {
+    }
+    
+    public func readCharacteristic(uuid: String, receiver: @escaping (Data?) -> Void) {
+        self.delegate.readCharacteristicReceiver = receiver
+        if let peripheral = self.delegate.peripheralArray.first(where: { uuid == "\($0.identifier)" }) {
+            peripheral.delegate = self.delegate
+            peripheral.discoverServices(nil)
+        } else {
+            receiver(nil)
+        }
+    }
+    
+    public func notifyCharacteristic(uuid: String, receiver: @escaping (Data?) -> Void) {
+        self.delegate.notifyCharacteristicReceiver = receiver
+        if let peripheral = self.delegate.peripheralArray.first(where: { uuid == "\($0.identifier)" }) {
+            peripheral.delegate = self.delegate
+            peripheral.discoverServices(nil)
+        } else {
+            receiver(nil)
+        }
+    }
+    
+    public func writeCharacteristic(uuid: String, data: Data) {
+        self.delegate.writeData = data
+        if let peripheral = self.delegate.peripheralArray.first(where: { uuid == "\($0.identifier)" }) {
+            peripheral.delegate = self.delegate
+            peripheral.discoverServices(nil)
+        }
+    }
+    
+    public func listenToStateEvents(receiver: @escaping (DeviceState) -> Void) {
+        self.delegate.stateReceiver = receiver
+    }
 }
 
 extension BluetoothManager.CBCDelegate: CBCentralManagerDelegate {
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         switch central.state {
         case .unknown:
-          	stateReceiver?(.unknown)
+            stateReceiver?(.unknown)
         case .resetting:
-          	stateReceiver?(.resetting)
+            stateReceiver?(.resetting)
         case .unsupported:
-          	stateReceiver?(.unsupported)
+            stateReceiver?(.unsupported)
         case .unauthorized:
-          	stateReceiver?(.unauthorized)
+            stateReceiver?(.unauthorized)
         case .poweredOff:
-          	stateReceiver?(.poweredOff)
+            stateReceiver?(.poweredOff)
         case .poweredOn:
-        	stateReceiver?(.poweredOn)
+            stateReceiver?(.poweredOn)
 
         @unknown default:
-        	stateReceiver?(.unknown)
+            stateReceiver?(.unknown)
         }
     }
   
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-	  let peripheralData = self.convertPeripheral(peripheral: peripheral)
-      if !self.peripheralArray.contains(peripheral) {
-          self.peripheralArray.append(peripheral)
+        let peripheralData = self.convertPeripheral(peripheral: peripheral)
+        if !self.peripheralArray.contains(peripheral) {
+            self.peripheralArray.append(peripheral)
 
-          receiver?(peripheralData)
-      }
+            receiver?(peripheralData)
+        }
     }
 
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-	    let peripheralData = self.convertPeripheral(peripheral: peripheral)
-	    receiver?(peripheralData)
-    }
-    
-    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
-	    let peripheralData = self.convertPeripheral(peripheral: peripheral)
+        let peripheralData = self.convertPeripheral(peripheral: peripheral)
         receiver?(peripheralData)
     }
     
-    private func convertPeripheral(peripheral: CBPeripheral) -> Peripheral{       
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        let peripheralData = self.convertPeripheral(peripheral: peripheral)
+        receiver?(peripheralData)
+    }
+    
+    private func convertPeripheral(peripheral: CBPeripheral) -> Peripheral{
         return Peripheral(name: peripheral.name, uuid: "\(peripheral.identifier)", isConnected: peripheral.state == .connected)
     }
 }
 
 extension BluetoothManager.CBCDelegate: CBPeripheralDelegate {
-  func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        for service in peripheral.services! {
+            peripheral.discoverCharacteristics(nil, for: service)
+        }
+    }
 
-  }
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        for characteristic in service.characteristics! {
+            let characteristic = characteristic as CBCharacteristic
 
-  func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
-  
-  }
+            if characteristic.properties.contains(.write) {
+                print("\(characteristic.uuid): properties contains .write")
+                if let writeData = writeData {
+                    peripheral.writeValue(writeData, for: characteristic, type: .withResponse)
+                }
+            }
+            
+            if characteristic.properties.contains(.read) {
+                print("\(characteristic.uuid): properties contains .read")
+                peripheral.readValue(for: characteristic)
+            }
+            
+            if characteristic.properties.contains(.notify) {
+                print("\(characteristic.uuid): properties contains .notify")
+                peripheral.setNotifyValue(true, for: characteristic)
+            }
+        }
+    }
 
-  func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
-  }
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        if characteristic.properties.contains(.write) {
+            
+        }
+        
+        if characteristic.properties.contains(.read) {
+            readCharacteristicReceiver?(characteristic.value!)
+        }
+        
+        if characteristic.properties.contains(.notify) {
+            notifyCharacteristicReceiver?(characteristic.value!)
+        }
+    }
 }
-
 
 #endif
--- Sources/FusionBluetooth_Common/BluetoothManager.swift
+++ Sources/FusionBluetooth_Common/BluetoothManager.swift
@@ -49,7 +49,7 @@ public enum DeviceState {
     case unknown // State unknown, update imminent.
     case unsupported // The platform doesn't support the Bluetooth Low Energy Central/Client role.
     case unauthorized // The application is not authorized to use the Bluetooth Low Energy role.
-    case poweredOn // Bluetooth is currently powered on and available to use.    
+    case poweredOn // Bluetooth is currently powered on and available to use.
     case poweredOff // Bluetooth is currently powered off.
     case resetting // The connection with the system service was momentarily lost, update imminent.
 }
@@ -62,14 +62,14 @@ public enum DeviceState {
  */
 public protocol BluetoothManagerProtocol {
 
-	/*
+    /*
      * @method requestAuthorization:
      *
      * @discussion Requests the ACCESS_FINE_LOCATION permission. For only Android
      */
     func requestAuthorization()
     
-	/*
+    /*
      * @method isSupporting:
      *
      * @discussion Returns whether the central device supports the Bluetooth.
@@ -81,82 +81,94 @@ public protocol BluetoothManagerProtocol {
      *
      * @discussion Whether or not the central is currently discovering.
      */
-	func isDiscovering() -> Bool
-	
-	/*
+    func isDiscovering() -> Bool
+    
+    /*
      * @method isEnabled:
      *
      * @discussion Returns whether the central device is enabled.
-     */     
-	func isEnabled() -> Bool
-	
-	/*
+     */
+    func isEnabled() -> Bool
+    
+    /*
      * @method startDiscovering:
      *
      * @param receiver Returns a Peripheral discovered.
      *
      * @discussion Start Discovering Peripherals.
      */
-	func startDiscovering(receiver: @escaping (Peripheral?) -> Void)
-	
-	/*
+    func startDiscovering(receiver: @escaping (Peripheral?) -> Void)
+    
+    /*
      * @method stopDiscovering:
      *
      * @discussion Stops scanning peripherals.
      */
-	func stopDiscovering()
-	
-	/*
+    func stopDiscovering()
+    
+    /*
      * @method enableBluetooth:
      *
      * @discussion Create the request to the user to activate the bluetooth. For only Android
      */
-	func enableBluetooth()
-		
-	/*
+    func enableBluetooth()
+        
+    /*
      * @method connectDevice:
      *
-     * @param uuid Peripheral identifier in iOS and Device Mac addres in Android 
+     * @param uuid Peripheral identifier in iOS and Device Mac addres in Android
      * @param receiver Returns the Peripheral connected.
      *
      * @discussion Connects a peripheral
      */
-	func connectDevice(uuid: String, receiver: @escaping (Peripheral?) -> Void)
-	
-	/*
+    func connectDevice(uuid: String, receiver: @escaping (Peripheral?) -> Void)
+    
+    /*
      * @method disconnectDevice:
      *
-     * @param uuid Peripheral identifier in iOS and Device Mac addres in Android 
+     * @param uuid Peripheral identifier in iOS and Device Mac addres in Android
      * @param receiver Returns the Peripheral disconnected.
      *
      * @discussion Disconnects a peripheral
      */
-	func disconnectDevice(uuid: String, receiver: @escaping (Peripheral?) -> Void)
-	
-	/*
-     * @method receiveMessage:
+    func disconnectDevice(uuid: String, receiver: @escaping (Peripheral?) -> Void)
+
+    /*
+     * @method writeCharacteristic:
      *
-     * @param message A message string received
+     * @param uuid Peripheral identifier in iOS and Device Mac addres in Android
+     * @param data A data to send
      *
-     * @discussion Receives a message to a peripheral.
+     * @discussion write a Characteristic to a peripheral.
      */
-	func receiveMessage(message: @escaping (String) -> Void)
-	
-	/*
+    func writeCharacteristic(uuid: String, data: Data)
+            
+    /*
+     * @method readCharacteristic:
+     *
+     * @param uuid Peripheral identifier in iOS and Device Mac addres in Android
+     * @param data A data received
+     *
+     * @discussion Read a Characteristic from a peripheral.
+     */
+    func readCharacteristic(uuid: String, receiver: @escaping (Data?) -> Void)
+    
+    /*
+     * @method notifyCharacteristic:
+     *
+     * @param uuid Peripheral identifier in iOS and Device Mac addres in Android
+     * @param data A data received
+     *
+     * @discussion Notify a Characteristic from a peripheral.
+     */
+    func notifyCharacteristic(uuid: String, receiver: @escaping (Data?) -> Void)
+    
+    /*
      * @method listenToStateEvents:
      *
      * @param receiver Returns the DeviceState. For only iOS
      *
      * @discussion Listen to state events of bluetooth device.
      */
-	func listenToStateEvents(receiver: @escaping (DeviceState) -> Void)
-		
-	/*
-     * @method sendMessage:
-     *
-     * @param message A message string to send
-     *
-     * @discussion Sends a message to a peripheral.
-     */
-    func sendMessage(message: String)
+    func listenToStateEvents(receiver: @escaping (DeviceState) -> Void)
 }
